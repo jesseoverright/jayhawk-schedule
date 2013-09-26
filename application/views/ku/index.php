dd65212974aa8ade09cd6741ea9fefe0c0bab34b
Jesse Overright <jesseoverright@gmail.com>
2013-09-26 10:26:29 -0500

    Added television field to create, edit, and views

--- application/views/ku/index.php
+++ application/views/ku/index.php
@@ -2,11 +2,11 @@
 <?php foreach ($ku_games as $game) : ?>
 	<div class="game">
 		<?php if ($game->result): ?>
-			<h2 class="final-score <?php echo $game->result; ?>">
-				<?php echo $game->score; ?> - <?php echo $game->opponent_score; ?>
+			<h2 class="final-score <?= $game->result; ?>">
+				<?= $game->score; ?> - <?= $game->opponent_score; ?>
 			</h2>
 		<?php endif; ?>
-		<h2><a href="/schedule/game/<?php echo $game->slug; ?>"><?php echo $game->opponent.' '.$game->mascot; ?></a>	</h2>
-		<p><span class="game-type"><?php echo $game->type; ?></span><?php echo $game->time; ?> <?php echo $game->formatted_date;?></p>
+		<h2><a href="/schedule/game/<?= $game->slug; ?>"><?= $game->opponent.' '.$game->mascot; ?></a>	</h2>
+		<p><span class="game-type"><?= $game->type; ?></span><?= $game->time; ?> <?= $game->formatted_date;?><?php if ($game->television != '') echo " on {$game->television}"; ?></p>
 	</div>
 <?php endforeach ?>
