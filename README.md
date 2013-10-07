2773dec4bdf211278668e2af22e3df9eb453d7f1
Jesse Overright <jesseoverright@gmail.com>
2013-10-07 17:55:20 -0500

    Updated display for home games

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 #Jayhawk Schedule
 
-This is a sandbox for codeignitor that also serves as a basketball scheduling too.
+The basics of this app is to keep track of the __Kansas Jayhawks 2013-13__ basketball schedule. It's a very simple application built in codeigniter. It allows a user to:
+
+- create games & opponents
+- set location
+- set television info 
+- add game time
+- update results for the year as games occur
 
 ![sample view of schedule](https://raw.github.com/jesseoverright/jayhawk-schedule/master/images/schedule-sample.png)
--- application/views/ku/game.php
+++ application/views/ku/game.php
@@ -1,6 +1,10 @@
 <div class="edit"><p><a href="/schedule/game/<?= $game->slug; ?>/edit">Edit game</a></p></div>
 <div id="single-game">
-	<h2>Kansas Jayhawks vs <?= $game->opponent.' '.$game->mascot; ?></h2>
+    <?php if ($game->location == "Allen Fieldhouse, Lawrence, KS") : ?>
+        <h2><?= $game->opponent.' '.$game->mascot; ?> at Kansas Jayhawks</h2>
+    <?php else : ?>
+        <h2>Kansas Jayhawks vs <?= $game->opponent.' '.$game->mascot; ?></h2>
+    <?php endif ?>
 	<?php if ($game->result): ?>
 		<h2 class="final-score <?= $game->result; ?>">
 			<?= $game->score; ?> - <?= $game->opponent_score; ?>
